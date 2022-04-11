bed29cf2eae7db52d4b16efddfe6c833895b35d8
volpertinger <merzlovnik@mail.ru>
2022-04-11 15:41:43 +0300

    работает нейронка с двумя слоями

--- Lab_04/main.py
+++ Lab_04/main.py
@@ -1,3 +1,10 @@
+from multiLayerNetwork import NeuralNetwork
+
 if __name__ == '__main__':
     # 2-1-2 input: (1,1,2) 10 * output: (2,2) -> output^ (0.2, 0.2)
-    print(0)
+    architecture = [2, 1, 2]
+    inputVector = [1, 1, 2]
+    outputVector = [0.2, 0.2]
+    neuralNetwork = NeuralNetwork(inputVector, outputVector, architecture, 0.3, 0.01)
+    neuralNetwork.teach()
+    print(neuralNetwork.getLogStr())
--- Lab_04/multiLayerNetwork.py
+++ Lab_04/multiLayerNetwork.py
@@ -1,4 +1,5 @@
-from math import exp
+from math import exp, sqrt
+from copy import deepcopy
 
 
 # значение функции активации
@@ -12,6 +13,19 @@ def getActivationDerivative(net):
 
 
 class NeuralNetwork:
+    class __Log:
+        def __init__(self, gen, weightsOpen, weightsHide, function, delta):
+            self.__gen = gen
+            self.__weightsOpen = weightsOpen
+            self.__weightsHide = weightsHide
+            self.__function = function
+            self.__delta = delta
+
+        def __str__(self):
+            return '-----Gen ' + str(self.__gen) + '\n' + 'WeightsOpen: ' + str(
+                self.__weightsOpen) + '\n' + 'WeightsHide: ' + str(self.__weightsHide) + '\n' + 'Function: ' + str(
+                self.__function) + '\n' + 'Delta: ' + str(self.__delta) + '\n'
+
     def __init__(self, inputVector, outputVector, architecture, norm=0.3, maxError=0.1):
         # вектор входных сигналов
         self.__inputVector = inputVector
@@ -31,6 +45,23 @@ class NeuralNetwork:
         self.__hideWeights = self.__getHideWeights()
         # веса между скрытым слоем и открытым слоем
         self.__openWeights = self.__getOpenWeights()
+        # показатель обученности сети
+        self.__isTrained = False
+        # лог обучение
+        self.__log = []
+
+    # добавляется лог эпохи
+    def __addLog(self, weightsOpen, weightsHide, function):
+        gen = len(self.__log)
+        delta = self.__getError()
+        self.__log.append(self.__Log(gen, weightsOpen, weightsHide, function, delta))
+
+    # функция выхода для логирования
+    def __getCurrentFunction(self):
+        result = []
+        for i in range(self.__openNeuronCount):
+            result.append(self.__getOpenOutput(i))
+        return result
 
     # веса между входами и нейронами скрытого слоя
     def __getHideWeights(self):
@@ -62,9 +93,8 @@ class NeuralNetwork:
 
     # считаем значение выхода в открытый нейрон заданного индекса
     def __getOpenOutput(self, index):
-        result = 0
-        # + 1 т.к. здесь тоже учитываются константы
-        for i in range(self.__hideNeuronCount + 1):
+        result = self.__openWeights[index][0]
+        for i in range(self.__hideNeuronCount):
             result += self.__openWeights[index][i] * self.__getHideOutput(i)
         return getActivationFunction(result)
 
@@ -100,3 +130,51 @@ class NeuralNetwork:
             # + 1 - смещение от константы
             self.__hideWeights[index][i + 1] += deltaNorm * getActivationDerivative(self.__getOpenOutput(index)) * \
                                                 self.__inputVector[i + 1]
+
+    # выполняется коррекция весов между всеми слоями
+    def __makeCorrection(self):
+        # заполняем массив дельт и сохраняем веса между скрытым и открытым слоями, чтобы потом произвести
+        # коррекцию весов между переменными и скрытым слоем
+        deltaArray = []
+        for i in range(self.__openNeuronCount):
+            deltaArray.append(self.__getDeltaOpen(i))
+        oldWeightsArray = deepcopy(self.__openWeights)
+
+        # коррекция весов между скрытым и открытым слоем
+        for i in range(self.__openNeuronCount):
+            self.__makeOpenCorrection(i, deltaArray[i])
+
+        # коррекция весов между переменными и скрытым слоем
+        for i in range(self.__hideNeuronCount):
+            self.__makeHideCorrection(i, deltaArray, oldWeightsArray)
+
+    # получение среднеквадратичной ошибки эпохи
+    def __getError(self):
+        totalError = 0
+        for i in range(self.__openNeuronCount):
+            totalError += self.__getDeltaOpen(i) ** 2
+        totalError = sqrt(totalError)
+        return totalError
+
+    # Поколение обучения. True - обучение завершено, False - не завершено
+    def __solveGeneration(self):
+        totalError = self.__getError()
+        self.__addLog(self.__openWeights, self.__hideWeights, self.__getCurrentFunction())
+        if totalError > self.__maxError:
+            self.__makeCorrection()
+            return False
+        return True
+
+    # производит полное обучение сети
+    def teach(self):
+        self.__isTrained = self.__solveGeneration()
+        while not self.__isTrained:
+            self.__isTrained = self.__solveGeneration()
+        return True
+
+    def getLogStr(self):
+        result = ''
+        for element in self.__log:
+            result += str(element)
+        result += '-----' + 'Trained: ' + str(self.__isTrained)
+        return result
