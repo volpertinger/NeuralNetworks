03af8cf34664e86b2947cb420edbb9223e435797
volpertinger <merzlovnik@mail.ru>
2022-04-24 23:47:58 +0300

    all done

--- lab_05/main.py
+++ lab_05/main.py
@@ -1,12 +1,39 @@
 from HopfildNetwork import HopfildNetwork
+from numpy import array
 
 # Асинхронный режим
 # Запоминаемые образы : 3 5 7
 
+# 3 = [1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1]
+# 5 = [1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1]
+# 7 = [1, 1, 1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1]
+
 if __name__ == '__main__':
     hopfildNetwork = HopfildNetwork([3, 5, 7])
-    # print(hopfildNetwork.getMatrixArrayStr())
     print(hopfildNetwork)
-    vector = [1, 1, 1, -1, -1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1]
-    print(vector)
-    print(hopfildNetwork.getOutputVector(vector))
+    vectors_initial = [[1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1]]
+    vectors_corrupted = [[-1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1],
+                         [1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1],
+                         [1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1],
+                         [1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1],
+                         [1, 1, 1, 1, 1, 1, -1, 1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1],
+                         [1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1],
+                         [1, 1, 1, 1, -1, -1, -1, 1, -1, -1, -1, 1, 1, -1, -1, 1, -1, -1, -1, 1],
+                         [1, 1, 1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, 1, 1, 1, -1, -1, -1, 1],
+                         [1, 1, 1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, 1, -1, 1]]
+    # проверка на полностью верных данных
+    print('Right vectors\n')
+    for vector in vectors_initial:
+        print(array(vector))
+        print(hopfildNetwork.getOutputVector(vector))
+        print()
+    # проверка частично искаженных векторов
+    print('Corrupted vectors\n')
+    for vector in vectors_corrupted:
+        print(array(vector))
+        print(hopfildNetwork.getOutputVector(vector))
+        print()
+    # print(vector)
+    # print(hopfildNetwork.getOutputVector(vector))
